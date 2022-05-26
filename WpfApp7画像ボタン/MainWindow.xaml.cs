e61655fb34e1567c43dafbeb6a62c1742d431691
Takato_Shioya <k019c1391@g.neec.ac.jp>
2022-05-26 14:13:58 +0900

    2022/05/26

---
+++ WpfApp7画像ボタン/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7画像ボタン
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        {
+            repeatText.Text = (int.Parse(repeatText.Text) + 1).ToString();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // 絶対URIを起動
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+            // イベントを処理済みとしてマークする
+            e.Handled = true;
+        }
+    }
+}
