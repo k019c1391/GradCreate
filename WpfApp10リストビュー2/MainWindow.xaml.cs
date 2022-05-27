07986eced798a4cc953cc0554cfe21ce207c035a
Takato_Shioya <k019c1391@g.neec.ac.jp>
2022-05-27 16:04:54 +0900

    2022/05/26

--- WpfApp10リストビュー2/MainWindow.xaml.cs
+++ WpfApp10リストビュー2/MainWindow.xaml.cs
@@ -15,7 +15,6 @@ using System.Windows.Shapes;
 
 namespace WpfApp10リストビュー2
 {
-    
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -32,6 +31,8 @@ namespace WpfApp10リストビュー2
             InitializeComponent();
 
             this.ZipRecords = new System.Collections.ObjectModel.ObservableCollection<ZipRecord>();
+            // 複数のスレッドからのアクセスを有効化
+            BindingOperations.EnableCollectionSynchronization(this.ZipRecords, new object());
 
             listView.DataContext = this.ZipRecords;
         }
@@ -85,7 +86,17 @@ namespace WpfApp10リストビュー2
             }
         }
 
-        private void opneMenu_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// CSVファイル読み込みタスク
+        /// </summary>
+        /// <param name="filePath">CSVファイルパス</param>
+        /// <returns></returns>
+        private Task ReadCsvTask(string filePath)
+        {
+            return Task.Run(() => { ReadCsv(filePath); });
+        }
+
+        private async void opneMenu_Click(object sender, RoutedEventArgs e)
         {
             var dlg = new Microsoft.Win32.OpenFileDialog();
 
@@ -95,10 +106,12 @@ namespace WpfApp10リストビュー2
 
             if (dlg.ShowDialog() == true)
             {
-                this.IsEnabled = false;
+                SetLoadingUI(true);
 
-                ReadCsv(dlg.FileName);
-                this.IsEnabled = true;
+                // CSV読み込み
+                await ReadCsvTask(dlg.FileName);
+
+                SetLoadingUI(false);
             }
         }
 
@@ -122,6 +135,40 @@ namespace WpfApp10リストビュー2
         {
             // 一覧をクリア
             this.ZipRecords.Clear();
+        }
+
+        /// <summary>
+        /// データ読み込み中の画面表示
+        /// </summary>
+        /// <param name="loading">データ読み込み中の時にtrue, 読み込み終わったらfalseを設定する</param>
+        private void SetLoadingUI(bool loading)
+        {
+            if (loading)
+            {
+                // 処理中
+
+                // 画面全体を無効化
+                this.IsEnabled = !loading;
+                // 処理中メッセージを表示
+                loadingText.Visibility = Visibility.Visible;
+                // リストを隠す
+                listView.Visibility = Visibility.Collapsed;
+                // タスクバーアイコンを処理中表示にする
+                taskbarInfo.ProgressState = System.Windows.Shell.TaskbarItemProgressState.Indeterminate;
+            }
+            else
+            {
+                // 処理終り
+
+                // 画面全体を有効化
+                this.IsEnabled = !loading;
+                // 処理中メッセージを隠す
+                loadingText.Visibility = Visibility.Collapsed;
+                // リストを表示
+                listView.Visibility = Visibility.Visible;
+                // タスクバーアイコンを通常に戻す
+                taskbarInfo.ProgressState = System.Windows.Shell.TaskbarItemProgressState.None;
+            }
         }
     }
 
